757676c1e4679fba17dfdff8c908f827333820b1
mnhss12 <37605170+mnhss12@users.noreply.github.com>
2020-11-24 00:31:06 +0900

    追加: 画面遷移管理用のインターフェースクラス追加

---
+++ DxLibTacticalGame/src/Screnn/ScreenImpl.cpp
@@ -0,0 +1,6 @@
+#include "ScreenImpl.h"
+
+namespace Screen
+{
+
+}
---
+++ DxLibTacticalGame/src/Screnn/ScreenImpl.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <climits>
+#include <memory>
+#include "Entity/Object.h"
+
+using namespace std;
+
+namespace Screen
+{
+	class ScreenImpl
+	{
+	public:
+		ScreenImpl() : isInited_(false) {};
+		~ScreenImpl() {};
+
+		/**
+		 * @fn
+		 * 初期処理
+		*/
+		virtual void init() = 0;
+
+		/**
+		 * @fn
+		 * イベント取得後のスクリーン更新処理
+		*/
+		virtual void updateByEvents(weak_ptr < Entity::Object> hitObjWp, int button, int eventType, int x, int y) = 0;
+		
+		/**
+		 * @fn
+		 * アニメーション終了後のスクリーン更新処理
+		*/
+		virtual void updateByAnimation() = 0;
+	private:
+		//! 初期処理が完了しているか
+		bool isInited_;
+	};
+}
