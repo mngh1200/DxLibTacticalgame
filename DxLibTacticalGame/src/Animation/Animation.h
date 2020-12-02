01f68c1a7789e36698cf05ea867db10919a98bd6
mnhss12 <37605170+mnhss12@users.noreply.github.com>
2020-12-03 02:03:30 +0900

    追加: アニメーションクラスの定義追加

--- DxLibTacticalGame/src/Animation/Animation.h
+++ DxLibTacticalGame/src/Animation/Animation.h
@@ -18,21 +18,42 @@ namespace Entity
 	{
 	public:
 		Animation() : 
-			calcFunk_{}, 
+			calcFunk_(Easing::Linear<float>),
 			frameCount_(0),
 			frameMax_(0),
-			y0_(0),
-			y1_(1)
+			delayFrame_(0),
+			direction_(NORMAL),
+			repeat_(0)
 		{};
 
-		Animation(int timeMs, int y1 = 1, int y0 = 0, function<float(float, float, float, float)> func = Easing::Linear<float>);
+		Animation(int timeMs, int direction = NORMAL, int repeat = 0, function<float(float, float, float, float)> func = Easing::Linear<float>, int delayMs = 0);
 
 		~Animation() {};
 
-		bool update(float* y);
-		bool update(int* y);
+		void increaseFrame(bool* isFin, bool* isUpdate, bool isIncreaseFrame = true);
+
+		void adjustFrame(int x, int y, int x0, int y0, int x1, int y1);
+		void adjustFrame(Shape* nowShape, Shape baseShape, float scale);
+
+		bool update(float* y, float y0 = 0, float y1 = 1, bool isIncreaseFrame = true);
+		bool update(int* y, int y0, int y1, bool isIncreaseFrame = true);
+
+		bool update(int* nowX, int* nowY, int x0, int y0, int x1, int y1, bool isIncreaseFrame = true);
+		bool update(Shape* nowShape, Shape baseShape, float scale, bool isIncreaseFrame = true);
+		bool update(int* nowColor, int r0, int g0, int b0, int r1, int g1, int b1, bool isIncreaseFrame = true);
+
+		enum Direction
+		{
+			NORMAL,				// 通常方向を実行する（初期値）
+			AlTERNATE,			// 通常方向と、逆方向を繰り返す
+			REVERSE,			// 毎回逆方向を実行する
+			AlTERNATE_REVERSE	// 通常方向と、逆方向を繰り返す（逆再生から始まる）
+		};
+
+		static const int REPEAT_INFINITE = -1;
 
 	private:
+
 		//! 変化量計算用の関数を保持する
 		function<float(float, float, float, float)> calcFunk_;
 
@@ -42,10 +63,14 @@ namespace Entity
 		//! 終了時点のフレーム数
 		float frameMax_;
 
-		//! 変化量の初期値
-		float y0_;
+		//! アニメーション開始遅延フレーム（値が0になった時点からアニメーションを開始する）
+		int delayFrame_;
 
-		//! 変化量の終了値
-		float y1_;
+		//! 実行する方向
+		int direction_;
+
+		//! リピート回数
+		int repeat_;
+
 	};
 }
