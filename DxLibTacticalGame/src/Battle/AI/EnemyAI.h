e7ec32f165f2af51aea2b62c41e90e311af53d83
mnhss12 <37605170+mnhss12@users.noreply.github.com>
2021-02-17 18:49:10 +0900

    改善: AIの調整

--- DxLibTacticalGame/src/Battle/AI/EnemyAI.h
+++ DxLibTacticalGame/src/Battle/AI/EnemyAI.h
@@ -62,7 +62,7 @@ namespace Battle
 		constexpr static int FORT_SCORE = 100; //! 砦マスの基本スコア
 		constexpr static int FORT_MOVE_SCORE_RATE = 3;	//! 砦マスの波及スコアの係数
 		constexpr static int SCORE_BY_FORT = (MAP_MASS_W + MAP_MASS_H - 2) * FORT_MOVE_SCORE_RATE; //! 砦から波及する基本スコアの加算
-		constexpr static int SCORE_BY_FRIEND = 5; //! 味方ユニットとの隣接スコア
+		constexpr static int SCORE_BY_FRIEND = 2; //! 味方ユニットとの隣接スコア係数
 
 		void setBaseScore(shared_ptr<Map> map);
 		void setBaseScoreByFort(shared_ptr<Map> map, int x, int y, int move, std::map<pair<int, int>, int>& tmpBaseScoreMap);
